d3588dcb4b60bb29b8b2dcbe9c30be8154e9639f
leoportogtr86 <52595646+leoportogtr86@users.noreply.github.com>
2020-05-19 07:18:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var draw = SVG().addTo('body').size(400,600)
-var synth = new Tone.Synth({oscillator: {type: 'sine'}}).toMaster()
+var synth = new Tone.Synth({oscillator: {type: 'sine'}}).toDestination()
 
 let start = document.querySelector('#start')
 let pad = document.querySelector('#pad')
@@ -12,6 +12,8 @@ start.onclick = function(){
    pad.play()
    n1.animate(t).move(0,700)
 }
+
+
 let n1 = draw.rect(80,80).fill('red')
 n1.radius(10)
 n1.move(0, -100)
@@ -73,6 +75,153 @@ n14.move(30, -100)
 let n15 = draw.rect(80, 80).fill('red')
 n15.radius(10)
 n15.move(0, -100)
+
+
+let n16 = draw.rect(80, 80).fill('#0080FF')
+n16.radius(10)
+n16.move(100, -100)
+
+let n17 = draw.rect(80, 80).fill('#0080FF')
+n17.radius(10)
+n17.move(100, -100)
+
+
+let n18 = draw.rect(80, 80).fill('green')
+n18.radius(10)
+n18.move(70, -100)
+
+
+let n19 = draw.rect(80, 80).fill('green')
+n19.radius(10)
+n19.move(70, -100)
+
+
+let n20 = draw.rect(80, 80).fill('yellow')
+n20.radius(10)
+n20.move(50, -100)
+
+
+let n21 = draw.rect(80, 80).fill('yellow')
+n21.radius(10)
+n21.move(50, -100)
+
+
+
+let n22 = draw.rect(80, 80).fill('orange')
+n22.radius(10)
+n22.move(20, -100)
+
+
+
+let n23 = draw.rect(80, 80).fill('orange')
+n23.radius(10)
+n23.move(20, -100)
+
+
+
+let n24 = draw.rect(80, 80).fill('#0080FF')
+n24.radius(10)
+n24.move(100, -100)
+
+let n25 = draw.rect(80, 80).fill('#0080FF')
+n25.radius(10)
+n25.move(100, -100)
+
+
+let n26 = draw.rect(80, 80).fill('green')
+n26.radius(10)
+n26.move(70, -100)
+
+
+let n27 = draw.rect(80, 80).fill('green')
+n27.radius(10)
+n27.move(70, -100)
+
+
+let n28 = draw.rect(80, 80).fill('yellow')
+n28.radius(10)
+n28.move(50, -100)
+
+
+let n29 = draw.rect(80, 80).fill('yellow')
+n29.radius(10)
+n29.move(50, -100)
+
+
+
+let n30 = draw.rect(80, 80).fill('orange')
+n30.radius(10)
+n30.move(20, -100)
+
+
+
+let n31 = draw.rect(80, 80).fill('orange')
+n31.radius(10)
+n31.move(20, -100)
+
+
+let n32 = draw.rect(80,80).fill('red')
+n32.radius(10)
+n32.move(0, -100)
+
+let n33 = draw.rect(80, 80).fill('red')
+n33.radius(10)
+n33.move(0, -100)
+
+let n34 = draw.rect(80, 80).fill('#0080FF')
+n34.radius(10)
+n34.move(100, -100)
+
+let n35 = draw.rect(80, 80).fill('#0080FF')
+n35.radius(10)
+n35.move(100, -100)
+
+let n36 = draw.rect(80, 80).fill('darkblue')
+n36.radius(10)
+n36.move(150, -100)
+
+let n37 = draw.rect(80, 80).fill('darkblue')
+n37.radius(10)
+n37.move(150, -100)
+
+let n38 = draw.rect(80, 80).fill('#0080FF')
+n38.radius(10)
+n38.move(100, -100)
+
+
+
+let n39 = draw.rect(80, 80).fill('#0080FF')
+n39.radius(10)
+n39.move(100, -100)
+
+let n40 = draw.rect(80, 80).fill('green')
+n40.radius(10)
+n40.move(70, -100)
+
+let n41 = draw.rect(80, 80).fill('green')
+n41.radius(10)
+n41.move(70, -100)
+
+let n42 = draw.rect(80, 80).fill('yellow')
+n42.radius(10)
+n42.move(50, -100)
+
+let n43 = draw.rect(80, 80).fill('yellow')
+n43.radius(10)
+n43.move(50, -100)
+
+let n44 = draw.rect(80, 80).fill('orange')
+n44.radius(10)
+n44.move(30, -100)
+
+let n45 = draw.rect(80, 80).fill('orange')
+n45.radius(10)
+n45.move(30, -100)
+
+let n46 = draw.rect(80, 80).fill('red')
+n46.radius(10)
+n46.move(0, -100)
+
 
 
 
@@ -220,7 +369,356 @@ n15.click(() => {
 
    n15.hide()
    synth.triggerAttackRelease('C4', '4n')
+   n16.animate(t).move(0,700)
+
 
 
 
 })
+
+
+
+n16.click(() => {
+
+   n16.hide()
+   synth.triggerAttackRelease('G4', '4n')
+   n17.animate(t).move(100,700)
+
+
+
+
+})
+
+
+n17.click(() => {
+
+   n17.hide()
+   synth.triggerAttackRelease('G4', '4n')
+   n18.animate(t).move(100,700)
+
+
+
+
+})
+
+
+n18.click(() => {
+
+   n18.hide()
+   synth.triggerAttackRelease('F4', '4n')
+   n19.animate(t).move(70,700)
+
+
+
+
+})
+
+
+n19.click(() => {
+
+   n19.hide()
+   synth.triggerAttackRelease('F4', '4n')
+   n20.animate(t).move(70,700)
+
+
+
+
+})
+
+
+n20.click(() => {
+
+   n20.hide()
+   synth.triggerAttackRelease('E4', '4n')
+   n21.animate(t).move(50,700)
+
+
+
+
+})
+
+
+
+n21.click(() => {
+
+   n21.hide()
+   synth.triggerAttackRelease('E4', '4n')
+   n22.animate(t).move(50,700)
+
+
+
+
+})
+
+
+n22.click(() => {
+
+   n22.hide()
+   synth.triggerAttackRelease('D4', '4n')
+   n23.animate(t).move(30,700)
+
+
+
+
+})
+
+
+n23.click(() => {
+
+   n23.hide()
+   synth.triggerAttackRelease('D4', '4n')
+   n24.animate(t).move(30,700)
+
+
+
+
+})
+
+
+n24.click(() => {
+
+   n24.hide()
+   synth.triggerAttackRelease('G4', '4n')
+   n25.animate(t).move(100,700)
+
+
+
+
+})
+
+
+n25.click(() => {
+
+   n25.hide()
+   synth.triggerAttackRelease('G4', '4n')
+   n26.animate(t).move(100,700)
+
+
+
+
+})
+
+
+n26.click(() => {
+
+   n26.hide()
+   synth.triggerAttackRelease('F4', '4n')
+   n27.animate(t).move(70,700)
+
+
+
+
+})
+
+
+n27.click(() => {
+
+   n27.hide()
+   synth.triggerAttackRelease('F4', '4n')
+   n28.animate(t).move(70,700)
+
+
+
+
+})
+
+
+n28.click(() => {
+
+   n28.hide()
+   synth.triggerAttackRelease('E4', '4n')
+   n29.animate(t).move(50,700)
+
+
+
+
+})
+
+
+n29.click(() => {
+
+   n29.hide()
+   synth.triggerAttackRelease('E4', '4n')
+   n30.animate(t).move(50,700)
+
+
+
+
+})
+
+
+n30.click(() => {
+   n30.hide()
+   synth.triggerAttackRelease('D4', '4n')
+   n31.animate(t).move(30,700)
+
+
+
+
+})
+
+
+n31.click(() => {
+
+   n31.hide()
+   synth.triggerAttackRelease('D4', '4n')
+   n32.animate(t).move(30,700)
+
+
+
+
+})
+
+
+
+n32.click(()=>{
+   
+   window.navigator.vibrate(200)
+   
+   n32.hide()
+   synth.triggerAttackRelease('C4', '4n')
+   n33.animate(t).move(0,700)
+})
+
+n33.click(()=>{
+   
+   window.navigator.vibrate(200)
+   
+   n33.hide()
+   synth.triggerAttackRelease('C4', '4n')
+   n34.animate(t).move(100,700)
+})
+
+n34.click(() => {
+   
+   window.navigator.vibrate(200)
+
+   n34.hide()
+   synth.triggerAttackRelease('G4', '4n')
+   n35.animate(t).move(100,700)
+   
+})
+
+n35.click(() => {
+   
+   window.navigator.vibrate(200)
+
+   n35.hide()
+   synth.triggerAttackRelease('G4', '4n')
+   n36.animate(t).move(100,700)
+   
+})
+
+n36.click(() => {
+   
+   window.navigator.vibrate(200)
+
+   n36.hide()
+   synth.triggerAttackRelease('A4', '4n')
+   n37.animate(t).move(100,700)
+   
+
+})
+
+n37.click(() => {
+   
+   window.navigator.vibrate(200)
+
+   n37.hide()
+   synth.triggerAttackRelease('A4', '4n')
+   n38.animate(t).move(100,700)
+
+
+})
+
+n38.click(() => {
+   
+   window.navigator.vibrate(200)
+
+   n38.hide()
+   synth.triggerAttackRelease('G4', '4n')
+   n39.animate(t).move(100,700)
+   
+
+
+})
+
+n39.click(() => {
+
+   n39.hide()
+   synth.triggerAttackRelease('G4', '4n')
+   n40.animate(t).move(70,700)
+
+
+})
+
+n40.click(() => {
+
+   n40.hide()
+   synth.triggerAttackRelease('F4', '4n')
+   n41.animate(t).move(70,700)
+
+
+})
+
+n41.click(() => {
+
+   n41.hide()
+   synth.triggerAttackRelease('F4', '4n')
+   n42.animate(t).move(70,700)
+
+
+
+})
+
+n42.click(() => {
+
+   n42.hide()
+   synth.triggerAttackRelease('E4', '4n')
+   n43.animate(t).move(70,700)
+
+
+
+})
+
+n43.click(() => {
+
+   n43.hide()
+   synth.triggerAttackRelease('E4', '4n')
+   n44.animate(t).move(30,700)
+
+
+
+})
+
+n44.click(() => {
+
+   n44.hide()
+   synth.triggerAttackRelease('D4', '4n')
+   n45.animate(t).move(30,700)
+
+
+
+})
+
+n45.click(() => {
+
+   n45.hide()
+   synth.triggerAttackRelease('D4', '4n')
+   n46.animate(t).move(0,700)
+
+
+
+})
+
+n46.click(() => {
+
+   n46.hide()
+   synth.triggerAttackRelease('C4', '4n')
+
+
+
+
+})
+
+
